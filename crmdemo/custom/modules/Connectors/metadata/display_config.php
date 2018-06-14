bdb6a3b69a1811727d88da1c10056b3468b23a3e
root <root@murano-upgrade.rtlabs.co.uk>
2018-06-14 11:05:30 +0400

    Uncomment the commented code for red flags + elastica 8.0 folder

--- crmdemo/custom/modules/Connectors/metadata/display_config.php
+++ crmdemo/custom/modules/Connectors/metadata/display_config.php
@@ -1,19 +1,28 @@
 <?php
-// created: 2018-06-13 13:52:39
+// created: 2015-01-11 07:10:26
 $modules_sources = array (
   'Accounts' => 
   array (
+    'ext_soap_hoovers' => 'ext_soap_hoovers',
+    'ext_rest_twitter' => 'ext_rest_twitter',
+    'ext_rest_insideview' => 'ext_rest_insideview',
   ),
   'Contacts' => 
   array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+    'ext_rest_insideview' => 'ext_rest_insideview',
   ),
   'Leads' => 
   array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+    'ext_rest_insideview' => 'ext_rest_insideview',
   ),
   'Prospects' => 
   array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
   ),
   'Opportunities' => 
   array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
   ),
 );
